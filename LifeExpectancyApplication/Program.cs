9eeea31bd41c5980c8b47ac7178865a0a35643b7
Varun Maharaj <User3@DESKTOP-6C65FAF>
2020-02-13 08:14:20 +0200

    Project initialisation

---
+++ LifeExpectancyApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LifeExpectancyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Life Expectancy App!");
+        }
+    }
+}
